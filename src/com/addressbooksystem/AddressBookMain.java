4783fba37c34930cb3974547b2a8824c4d8e3ac3
Deepraj-devikar <deeprajdevikar19@gmail.com>
2022-09-02 16:50:00 +0530

    REFACTOR: multiple persons details

--- src/com/addressbooksystem/AddressBookMain.java
+++ src/com/addressbooksystem/AddressBookMain.java
@@ -9,24 +9,28 @@ public class AddressBookMain {
 		System.out.println("Welcome to Address Book Program");
 		Scanner scanner = new Scanner(System.in);
 		AddressBook addressBook = new AddressBook();
-		System.out.println("Enter person details: ");
-		System.out.print("First Name : ");
-		firstName = scanner.nextLine();
-		System.out.print("Last Name : ");
-		lastName = scanner.nextLine();
-		System.out.print("Address : ");
-		address = scanner.nextLine();
-		System.out.print("City : ");
-		city = scanner.nextLine();
-		System.out.print("State : ");
-		state = scanner.nextLine();
-		System.out.print("Zip : ");
-		zip = scanner.nextLine();
-		System.out.print("Phone Number : ");
-		phoneNumber = scanner.nextLine();
-		System.out.print("Email : ");
-		eamil = scanner.nextLine();
-		addressBook.addContact(firstName, lastName, address, city, state, zip, phoneNumber, eamil);
+		System.out.println("Enter 4 person details: ");
+		for (int number = 1; number <= 4; number++) {
+			System.out.println("");
+			System.out.println("Person "+number+" : ");
+			System.out.print("First Name : ");
+			firstName = scanner.nextLine();
+			System.out.print("Last Name : ");
+			lastName = scanner.nextLine();
+			System.out.print("Address : ");
+			address = scanner.nextLine();
+			System.out.print("City : ");
+			city = scanner.nextLine();
+			System.out.print("State : ");
+			state = scanner.nextLine();
+			System.out.print("Zip : ");
+			zip = scanner.nextLine();
+			System.out.print("Phone Number : ");
+			phoneNumber = scanner.nextLine();
+			System.out.print("Email : ");
+			eamil = scanner.nextLine();
+			addressBook.addContact(firstName, lastName, address, city, state, zip, phoneNumber, eamil);	
+		}
 		addressBook.showContacts();
 		scanner.close();
 	}
